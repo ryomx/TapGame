eff7258b42abb78fe4cb51b35bf84a3d1eb4c1bc
RyotaPw <matchaflavor@gmail.com>
2016-05-04 18:35:27 +0900

    first commit

---
+++ Scripts/AutoDestroyEffect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestroyEffect : MonoBehaviour {
+
+	ParticleSystem particle;
+
+	// Use this for initialization
+	void Start () {
+		particle = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// パーティクルの生成が終了したらGameObjectを削除
+		if (!particle.isPlaying) {
+			Destroy(gameObject);
+		}
+	}
+}
