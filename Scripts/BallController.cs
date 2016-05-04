51cb5eb8ce93897012165cb4fd1d2332ced3a012
RyotaPw <matchaflavor@gmail.com>
2016-05-04 18:36:11 +0900

    first commit

---
+++ Scripts/BallController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+//	public GameObject effectPrefab;
+//	public Vector3 effectRotation;
+	public float fallSpeed = 1.0f;
+//	p	rivate bool isTapSuccess = false;
+
+	void FixedUpdate ()
+	{
+		Vector3 moveDirection = transform.position;
+		// FixedUpdate()内で移動計算をする際、Time.deltaTimeの乗算は不要
+		//moveDirection.y -= fallSpeed * Time.deltaTime;
+		moveDirection.y -= fallSpeed;
+
+		transform.position = moveDirection;
+	}
+
+	public void setFallSpeed(float speed){
+		fallSpeed = speed;
+	}
+
+//	public void setTapSuccess(){
+//		isTapSuccess = true;
+//	}
+//
+//	public bool getTapSuccess(){
+//		return this.isTapSuccess;
+//	}
+}
